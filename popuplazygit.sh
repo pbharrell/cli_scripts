a22e480601e7bf4c35db7978c376fe079b5ff412
Preston Harrell <preston.harrell@garmin.com>
2026-05-20 11:16:25 -0500

    Fix popup lazygit to fix new tmux errors

--- popuplazygit.sh
+++ popuplazygit.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
-if [ "$(tmux display-message -p -F "#{session_name}")" = "popup" ];then 
-    return
-elif [ "$(tmux display-message -p -F "#{session_name}")" = "copilot" ];then 
-    return
-elif [ "$(tmux display-message -p -F "#{session_name}")" = "lazygit" ];then
+current_session="$(tmux display-message -p '#{session_name}')"
+if [ "$current_session" = "popup" ];then 
+    echo "Can't open lazygit in popup!"
+    exit
+elif [ "$current_session" = "copilot" ];then 
+    echo "Can't open lazygit in copilot!"
+    exit
+elif [ "$current_session" = "lazygit" ];then
     tmux switch -l
-else
-    tmux switch -t lazygit || (tmux new -d -s lazygit -c '#{pane_current_path}' 'tmux set-option status off; lazygit' && tmux switch -t lazygit)
-    tmux set-option -t lazygit detach-on-destroy off
+    exit
 fi
+ 
+tmux switch -t lazygit &>/dev/null
+if [[ "$?" == "0" ]]; then
+    exit
+fi
+
+tmux new -d -s lazygit -c '#{pane_current_path}' 'lazygit'
+tmux set-option -t lazygit status off
+tmux set-option -t lazygit detach-on-destroy off
+tmux switch -t lazygit
